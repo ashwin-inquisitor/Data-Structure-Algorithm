f0e50a413b13785a8277ce4b0a83cface1df5abe
ashwin-inquisitor <ashwinkr019@gmail.com>
2025-10-22 08:28:21 +0530

    code updated

--- 1.c
+++ 1.c
@@ -4,14 +4,15 @@
 
 int main(){
     int num = 1;
-    char character ='A';
-    float pie = 3.14f;
-    long long biginteger= 8161351742;
+    int *ptr;
 
-    printf("Value of integer: %d. Address of integer: %u.\n", num, &num);
-    printf("Value of Character: %c. Address of Character: %u.\n", character, &character);
-    printf("Value of real: %f. Address of real: %u.\n", pie, &pie);
-    printf("Value of biginteger: %lld. Address of bidinteger: %u.\n", biginteger, &biginteger);
+    ptr = &num;
+
+    printf("Address of num: %d.\n", &num);
+    printf("Value of num: %d.\n", num);
+    printf("Address of ptr: %d.\n", &ptr);
+    printf("Value of ptr: %d.\n", ptr);
+    printf("Value pointed by ptr: %d.\n", *ptr);
 
     return 0;
 }
